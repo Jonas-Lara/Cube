edc5545647ffd19747ca88958b2e37b1a6d844ff
Jonas Lara <adrianlr2515@outlook.com>
2023-10-31 21:05:01 -0600

    UPDATE

--- cube-1.cpp
+++ cube-1.cpp
@@ -1,10 +1,7 @@
 #include <cmath>
 #include <iostream>
-#include <cstring>
-#include <thread>
-#include <chrono>
 #include <vector>
-// #include <emscripten.h>
+#include <emscripten.h>
 
 float A, B, C;
 
@@ -25,18 +22,18 @@ int xp, yp;
 int idx;
 
 float calculateX(int i, int j, int k) {
-    return j * std::sin(A) * std::sin(B) * std::cos(C) - k * std::cos(A) * std::sin(B) * std::cos(C) +
-        j * std::cos(A) * std::sin(C) + k * std::sin(A) * std::sin(C) + i * std::cos(B) * std::cos(C);
+    return j * sinf(A) * sinf(B) * cosf(C) - k * cosf(A) * sinf(B) * cosf(C) +
+        j * cosf(A) * sinf(C) + k * sinf(A) * sinf(C) + i * cosf(B) * cosf(C);
 }
 
 float calculateY(int i, int j, int k) {
-    return j * std::cos(A) * std::cos(C) + k * std::sin(A) * std::cos(C) -
-        j * std::sin(A) * std::sin(B) * std::sin(C) + k * std::cos(A) * std::sin(B) * std::sin(C) -
-        i * std::cos(B) * std::sin(C);
+    return j * cosf(A) * cosf(C) + k * sinf(A) * cosf(C) -
+        j * sinf(A) * sinf(B) * sinf(C) + k * cosf(A) * sinf(B) * sinf(C) -
+        i * cosf(B) * sinf(C);
 }
 
 float calculateZ(int i, int j, int k) {
-    return k * std::cos(A) * std::cos(B) - j * std::sin(A) * std::cos(B) + i * std::sin(B);
+    return k * cosf(A) * cosf(B) - j * sinf(A) * cosf(B) + i * sinf(B);
 }
 
 void calculateForSurface(float cubeX, float cubeY, float cubeZ, int ch) {
@@ -44,7 +41,7 @@ void calculateForSurface(float cubeX, float cubeY, float cubeZ, int ch) {
     y = calculateY(cubeX, cubeY, cubeZ);
     z = calculateZ(cubeX, cubeY, cubeZ) + distanceFromCam;
 
-    ooz = 1 / z;
+    ooz = 1.0f / z;
 
     xp = static_cast<int>(width / 2 + horizontalOffset + K1 * ooz * x * 2);
     yp = static_cast<int>(height / 2 + K1 * ooz * y);
@@ -58,16 +55,16 @@ void calculateForSurface(float cubeX, float cubeY, float cubeZ, int ch) {
     }
 }
 
-int main() {
-    std::cout << "\x1b[2J";
-    while (true) {
+extern "C" {
+    EMSCRIPTEN_KEEPALIVE
+    void update() {
         std::fill(buffer.begin(), buffer.end(), backgroundASCIICode);
         std::fill(zBuffer.begin(), zBuffer.end(), 0.0f);
         cubeWidth = 20;
         horizontalOffset = -2 * cubeWidth;
         // first cube
-        for (auto cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
-            for (auto cubeY = -cubeWidth; cubeY < cubeWidth; cubeY += incrementSpeed) {
+        for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
+            for (float cubeY = -cubeWidth; cubeY < cubeWidth; cubeY += incrementSpeed) {
                 calculateForSurface(cubeX, cubeY, -cubeWidth, '@');
                 calculateForSurface(cubeWidth, cubeY, cubeX, '$');
                 calculateForSurface(-cubeWidth, cubeY, -cubeX, '~');
@@ -79,8 +76,8 @@ int main() {
         cubeWidth = 10;
         horizontalOffset = 1 * cubeWidth;
         // second cube
-        for (auto cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
-            for (auto cubeY = -cubeWidth; cubeY < cubeWidth; cubeY += incrementSpeed) {
+        for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
+            for (float cubeY = -cubeWidth; cubeY < cubeWidth; cubeY += incrementSpeed) {
                 calculateForSurface(cubeX, cubeY, -cubeWidth, '@');
                 calculateForSurface(cubeWidth, cubeY, cubeX, '$');
                 calculateForSurface(-cubeWidth, cubeY, -cubeX, '~');
@@ -92,8 +89,8 @@ int main() {
         cubeWidth = 5;
         horizontalOffset = 8 * cubeWidth;
         // third cube
-        for (auto cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
-            for (auto cubeY = -cubeWidth; cubeY < cubeWidth; cubeY += incrementSpeed) {
+        for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
+            for (float cubeY = -cubeWidth; cubeY < cubeWidth; cubeY += incrementSpeed) {
                 calculateForSurface(cubeX, cubeY, -cubeWidth, '@');
                 calculateForSurface(cubeWidth, cubeY, cubeX, '$');
                 calculateForSurface(-cubeWidth, cubeY, -cubeX, '~');
@@ -102,17 +99,11 @@ int main() {
                 calculateForSurface(cubeX, cubeWidth, cubeY, '+');
             }
         }
-        std::cout << "\x1b[H";
-        for (int k = 0; k < width * height; k++) {
-            std::cout << (k % width ? buffer[k] : '\n');
-        }
-
-        A += 0.05;
-        B += 0.05;
-        C += 0.01;
-
-        // Sleep to control animation speed
-        std::this_thread::sleep_for(std::chrono::milliseconds(20)); // Adjust as needed
     }
+}
+
+int main() {
+    std::cout << "\x1b[2J";
+    emscripten_set_main_loop(update, 0, 1);
     return 0;
 }
